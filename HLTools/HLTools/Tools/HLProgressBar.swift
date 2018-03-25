17c62bce928ba3be0412c5acb1b5f60ff87bfc3e
121354692 <121354692@qq.com>
2018-03-26 00:28:33 +0800

    *绘制一个CircleProgressBar v1.0.0 (build 2)

---
+++ HLTools/HLTools/Tools/HLProgressBar.swift
@@ -0,0 +1,13 @@
+//
+//  HLProgressBar.swift
+//  HLTools
+//
+//  Created by 刘宏立 on 2018/3/25.
+//  Copyright © 2018年 lhl. All rights reserved.
+//
+
+import UIKit
+
+class HLProgressBar: NSObject {
+
+}
--- HLTools/HLTools/ViewController.swift
+++ HLTools/HLTools/ViewController.swift
@@ -9,17 +9,48 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let shapelayer = CAShapeLayer()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        setupUI()
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    
+    func setupUI() {
+        
+        let center = view.center
+        let trackLayer = CAShapeLayer()
+        let circularPath = UIBezierPath(arcCenter: center, radius: 100, startAngle: -CGFloat.pi/2, endAngle: 2*CGFloat.pi, clockwise: true)
+        trackLayer.path = circularPath.cgPath
+        trackLayer.strokeColor = UIColor.lightGray.cgColor
+        trackLayer.lineWidth = 10.0
+        trackLayer.fillColor = UIColor.clear.cgColor
+        trackLayer.lineCap = kCALineCapRound
+        view.layer.addSublayer(trackLayer)
+        
+        shapelayer.path = circularPath.cgPath
+        shapelayer.strokeColor = UIColor.red.cgColor
+        shapelayer.lineWidth = 10.0
+        shapelayer.fillColor = UIColor.clear.cgColor
+        shapelayer.strokeEnd = 0
+        shapelayer.lineCap = kCALineCapRound
+        view.layer.addSublayer(shapelayer)
+        
+        view.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(handleTap)))
+        
     }
-
-
+    
+    @objc private func handleTap() {
+        print("tap to animate stroke")
+        let basicAnimation = CABasicAnimation(keyPath: "strokeEnd")
+        basicAnimation.toValue = 1
+        basicAnimation.duration = 2
+        basicAnimation.fillMode = kCAFillModeForwards
+        basicAnimation.isRemovedOnCompletion = false
+        shapelayer.add(basicAnimation, forKey: "urSoBasic")
+    }
 }
 
