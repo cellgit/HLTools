5cf3cce11aa77763db59e6a6531067015f3b7c86
liuhongli <liuhongli@liuhonglis-macbook-pro.local>
2018-04-08 10:48:16 +0800

    设置popoverView的sourceRect

--- HLTools/HLTools/Tools/Popover/HLPopoverViewController.swift
+++ HLTools/HLTools/Tools/Popover/HLPopoverViewController.swift
@@ -11,16 +11,26 @@ import UIKit
 class HLPopoverViewController: UIViewController, UIPopoverPresentationControllerDelegate {
     @IBOutlet weak var PopoverButton: UIButton!
     
+    open var popoverSourceRect:CGRect = CGRect(x: 0, y: 0, width: 0, height: 0)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
 //        self.navigationItem.rightBarButtonItem = UIBarButtonItem.init(title: "Popover", style: .plain, target: self, action: #selector(evt_click_popover_action))
+        
     }
 
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == "showPopover" {
             let popoverViewController = segue.destination
+            PopoverButton.layer.anchorPoint = CGPoint(x: 0.5, y: 0.5)
             popoverViewController.popoverPresentationController?.delegate = self
+            let popVC:UIPopoverPresentationController = popoverViewController.popoverPresentationController!;
+            popVC.delegate = self;
+            popVC.permittedArrowDirections = UIPopoverArrowDirection.up
+            PopoverButton.layer.anchorPoint = CGPoint(x: 0.5, y: 0.5)
+            popoverSourceRect = CGRect(x: 20, y: 20, width: 10, height: 10)
+            popVC.sourceRect = popoverSourceRect
         }
     }
     
