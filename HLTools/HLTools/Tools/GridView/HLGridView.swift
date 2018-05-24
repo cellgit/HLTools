1f5c75001075dc6ef9d54f0612baa2c52d42fad8
121354692 <121354692@qq.com>
2018-05-24 22:59:29 +0800

    gridview

--- HLTools/HLTools/Tools/GridView/HLGridView.swift
+++ HLTools/HLTools/Tools/GridView/HLGridView.swift
@@ -8,84 +8,73 @@
 
 import UIKit
 
-struct HLGridViewStruct {
-    var views = [UIButton]()
-}
 
 class HLGridView: UIView {
     
-    var views = [UIButton]()
-    
-    convenience init(frame: CGRect, views: [UIButton]) {
-        self.init()
-        self.frame = frame
-        setupUI(views: views)
-        
-        self.views = views
-    }
+    var views = [UIButton]()            // 格子的按钮数组，可获取格子的个数
+    var column: Int = 0                 // 每行格子的个数
+    var containerWidth: CGFloat = 0     // 格子父视图的宽度
+    var separatorWidth: CGFloat = 0     // 格子的间距
+    var gridHeight: CGFloat = 0         // 每个格子的高度
+    var gridViewColor: UIColor? = UIColor.init(red: 225.0 / 255.0, green: 225.0 / 255.0, blue: 225.0 / 255.0, alpha: 1.0)       //gridView backgroundColor
     
     override init(frame: CGRect) {
         super.init(frame: frame)
-        
     }
     
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
-    public var subviewHeight: CGFloat = 0 {
-        willSet {}
-        didSet {
-            
-            setupUI(views: views)
-        }
+    override func layoutSubviews() {
+        self.containerWidth = frame.size.width
+        setupUI(views: views)
     }
     
     func setupUI(views: [UIButton]) {
-        self.backgroundColor = .cyan
-        let separatorWidth: CGFloat = 1
-        let columnCount: Int = 10
-        let subviewCount = views.count
-        let rowCount = subviewCount / columnCount + (subviewCount % columnCount > 0 ? 1 : 0)
-        let subviewWidth = floor(self.bounds.size.width - separatorWidth * CGFloat(columnCount - 1)) / CGFloat(columnCount)
-        self.frame.size.height = CGFloat(rowCount) * subviewHeight + CGFloat(rowCount-1) * separatorWidth
-        
+        self.backgroundColor = gridViewColor
+        guard column != 0 else { return }
+        let gridViewCount = views.count
+        let rowCount = gridViewCount / column + (gridViewCount % column > 0 ? 1 : 0)
+        self.frame.size.height = CGFloat(rowCount) * gridHeight + CGFloat(rowCount-1) * separatorWidth
+        let subviewWidth = floor(containerWidth - separatorWidth * CGFloat(column - 1)) / CGFloat(column)
         for item in self.subviews {
             if item.isKind(of: UIView.self) { item.removeFromSuperview() }
         }
-        
         for row in 0 ..< views.count {
-            for column in 0 ..< columnCount {
-                let index = row * columnCount + column
+            for columnIdx in 0 ..< column {
+                let index = row * column + columnIdx
                 guard index < views.count else {
                     continue
                 }
-                let isLastColumn = column == columnCount - 1
+                let isLastColumn = columnIdx == column - 1
                 let isLastRow = row == rowCount - 1
-                let offset_x = CGFloat(column) * CGFloat(subviewWidth + separatorWidth)
-                let offset_y = (subviewHeight + separatorWidth) * CGFloat(row)
+                let offset_x = CGFloat(columnIdx) * CGFloat(subviewWidth + separatorWidth)
+                let offset_y = (gridHeight + separatorWidth) * CGFloat(row)
+                views[index].backgroundColor = UIColor.white
+                
                 if index%2 == 0 {
                     views[index].backgroundColor = .red
                 }
                 else {views[index].backgroundColor = .lightGray}
-                views[index].action { (sender) in
-                    //
-                    sender.tag = index
-                    print("====tag==== \(sender.tag)")
-                }
-                views[index].frame = CGRect(x: offset_x, y: offset_y, width: subviewWidth, height: subviewHeight)
+                
+                //                views[index].action { (sender) in
+                ////                    sender.tag = index
+                //                    print("====tag==== \(sender.tag)")
+                ////                    self.delegate?.sw_grid_button_action(sender: sender)
+                //                }
+                views[index].frame = CGRect(x: offset_x, y: offset_y, width: subviewWidth, height: gridHeight)
                 
                 if isLastColumn {
                     // 每行最后一个item要占满剩余空间，否则可能因为strecthColumnWidth不精确导致右边漏空白
-                    let v = CGFloat(columnCount - 1)
+                    let v = CGFloat(column - 1)
                     views[index].frame.size.width = self.bounds.size.width - subviewWidth * v - separatorWidth * v
                 }
                 if isLastRow {
                     // 最后一行的item要占满剩余空间，避免一些计算偏差
                     let h = rowCount - 1
-                    views[index].frame.size.height = self.bounds.size.height - subviewHeight * CGFloat(h) - separatorWidth * CGFloat(h)
+                    views[index].frame.size.height = self.bounds.size.height - gridHeight * CGFloat(h) - separatorWidth * CGFloat(h)
                 }
-                
                 self.addSubview(views[index])
             }
         }
--- HLTools/HLTools/Tools/GridView/HLGridViewExampleVC.swift
+++ HLTools/HLTools/Tools/GridView/HLGridViewExampleVC.swift
@@ -12,7 +12,7 @@ class HLGridViewExampleVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        self.title = "GridViewController"
         self.view.backgroundColor = .white
         
         setupUI()
@@ -25,15 +25,49 @@ class HLGridViewExampleVC: UIViewController {
     
     func setupUI() {
         var buttons = [UIButton]()
-        for i in 0 ..< 20 {
+        for i in 0 ..< 8 {
             let button = UIButton.init(type: .custom)
             button.tag = i
             buttons.append(button)
+            buttons[i].addTarget(self, action: #selector(evt_click_action(sender:)), for: .touchUpInside)
         }
-        let screenWidth = UIScreen.main.bounds.size.width
-        let gridView = HLGridView.init(frame: .zero, views: buttons)
-        gridView.frame = CGRect(x: 0, y: 100, width: screenWidth, height: 0)
-        gridView.subviewHeight = 20
+        
+        let gridView = HLGridView.init(frame: .zero)
+        gridView.frame = CGRect(x: 0, y: 100, width: UIScreen.main.bounds.size.width, height: 0)
+        gridView.gridHeight = 96
+        gridView.column = 3
+        gridView.views = buttons
+        gridView.separatorWidth = 0.34
+        gridView.gridViewColor = UIColor.white
         self.view.addSubview(gridView)
     }
+    
+    
+    @objc func evt_click_action(sender: UIButton) {
+        print("====tag==== \(sender.tag)")
+        // 如果是在 cell 中布局需要先计算总高度,设置cell的高度,并且为cell设置identifier,点击时传递sender和identifier
+        // delegate?.sw_wealth_button_action(sender: sender, identifier: self.identifier)
+        
+        //在一般的view中使用
+        self.navigationController?.pushViewController(HLGridViewPushExampleVC(), animated: true)
+        
+        
+    }
 }
+
+class HLGridViewPushExampleVC: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.title = "GridView跳转"
+        
+        self.view.backgroundColor = .white
+        
+    }
+    
+    
+}
+
+
+
+
