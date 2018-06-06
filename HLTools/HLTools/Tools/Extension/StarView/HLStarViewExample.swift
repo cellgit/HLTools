70d4091bdea80c3484aae35b119f9d2d313a0ea2
121354692 <121354692@qq.com>
2018-06-06 22:32:39 +0800

    *修改评分功能示例

--- HLTools/HLTools/Tools/Extension/StarView/HLStarViewExample.swift
+++ HLTools/HLTools/Tools/Extension/StarView/HLStarViewExample.swift
@@ -8,37 +8,49 @@
 
 import UIKit
 
-class HLStarViewExample: UIViewController,EvaluateStarViewDelegate {
-    func evaluateStarView(_ evaluateStarView: EvaluateStarView, progressChangedTo progress: Float) {
-        //
-        print("===== \(progress)")
-    }
-    
+class HLStarViewExample: UIViewController {
     
     @IBOutlet weak var starContainerView: UIView!
     
+    var scoreLabel:UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupUI()
         
         scoreStarView()
-        
         evaluateScoreStarView()
     }
     
-    
-    
-    func scoreStarView() {
-        // 一句话设置评分星星
-        starContainerView.hl_starView(score: 4.6, starWidth: 11)
+    func setupUI() {
+        scoreLabel = UILabel.init(frame: CGRect(x: 0, y: 220, width: ScreenWidth, height: 30))
+        scoreLabel.text = "点击或滑动下面的星星进行评分"
+        self.view.addSubview(scoreLabel)
+        scoreLabel.textAlignment = .center
+        scoreLabel.textColor = UIColor.gray
     }
     
+    
+    /// 一句话设置评分星星
+    func scoreStarView() {
+        starContainerView.hl_starView(score: 4.6, starWidth: 11)
+    }
+}
+
+extension HLStarViewExample: EvaluateStarViewDelegate {
+    /// edit star score, via tap and pan gesture
     func evaluateScoreStarView() {
-        let starView = EvaluateStarView(frame: CGRect(x: 50, y: 300, width: 110, height: 22))
+        
+        
+        let starView = EvaluateStarView(frame: CGRect(x: (ScreenWidth - 110) / 2 , y: 260, width: 110, height: 22))
         starView.delegate = self
         view.addSubview(starView)
     }
-    
-    
-    
-
+    func evaluateStarView(_ evaluateStarView: EvaluateStarView, progressChangedTo progress: Float) {
+        print("===== \(progress)")
+        scoreLabel.text = String(progress * 5)
+    }
 }
+
+
