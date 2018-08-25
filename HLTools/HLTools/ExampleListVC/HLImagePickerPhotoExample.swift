694fd84b46ceeafc0395b077696d82f8b1f169b2
121354692 <121354692@qq.com>
2018-08-25 23:02:52 +0800

    YPImagePicker SigleVideo Multiple Photo and Video

--- HLTools/HLTools/ExampleListVC/HLImagePickerPhotoExample.swift
+++ HLTools/HLTools/ExampleListVC/HLImagePickerPhotoExample.swift
@@ -11,19 +11,37 @@ import YPImagePicker
 
 class HLImagePickerPhotoExample: UIViewController {
     
-    
     @IBOutlet weak var imgView: UIImageView!
-    
     @IBOutlet weak var button: UIButton!
+    @IBOutlet weak var sigleVideoBtn: UIButton!
+    @IBOutlet weak var multiSelectionBtn: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        buttonAction()
     }
 
     
     @IBAction func buttonAction(_ sender: UIButton) {
         
+        siglePhoto()
+    }
+    
+    func buttonAction() {
+        sigleVideoBtn.action { (sender) in
+            self.sigleVideo()
+        }
+        multiSelectionBtn.action { (sender) in
+            self.multiSelection()
+        }
+    }
+    
+    
+}
+
+extension HLImagePickerPhotoExample {
+    func siglePhoto() {
         let picker = YPImagePicker()
         picker.didFinishPicking { [unowned picker] items, _ in
             if let photo = items.singlePhoto {
@@ -40,8 +58,61 @@ class HLImagePickerPhotoExample: UIViewController {
         }
         present(picker, animated: true, completion: nil)
     }
-    
-    
-    
-
 }
+
+extension HLImagePickerPhotoExample {
+    func sigleVideo() {
+        // Here we configure the picker to only show videos, no photos.
+        var config = YPImagePickerConfiguration()
+        config.screens = [.library, .video]
+        config.library.mediaType = .video
+        
+        let picker = YPImagePicker(configuration: config)
+        picker.didFinishPicking { [unowned picker] items, _ in
+            if let video = items.singleVideo {
+                print("video.fromCamera=== \(video.fromCamera)")
+                print("video.thumbnail==== \(video.thumbnail)")
+                print("video.url========== \(video.url)")
+                self.imgView.image = video.thumbnail
+            }
+            picker.dismiss(animated: true, completion: nil)
+        }
+        present(picker, animated: true, completion: nil)
+    }
+}
+
+extension HLImagePickerPhotoExample {
+    func multiSelection() {
+        var config = YPImagePickerConfiguration()
+        config.library.maxNumberOfItems = 3
+        
+        let picker = YPImagePicker(configuration: config)
+        
+        picker.didFinishPicking { [unowned picker] items, cancelled in
+            for item in items {
+                switch item {
+                case .photo(let photo):
+                    print("photo=== \(photo)")
+                    self.imgView.image = photo.image
+                case .video(let video):
+                    print("video=== \(video)")
+                    self.imgView.image = video.thumbnail
+                }
+            }
+            
+            if cancelled {
+                print("Picker was canceled")
+            }
+            
+            picker.dismiss(animated: true, completion: nil)
+        }
+        
+        
+        
+        present(picker, animated: true, completion: nil)
+    }
+}
+
+
+
+
